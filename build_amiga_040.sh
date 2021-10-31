e6fb4d6a0cc1ca2a58ea03f27cd04074148d57cb
BSzili <col.lawrence@gmail.com>
2021-10-31 16:17:00 +0100

    update the cross-compile scripts

--- build_amiga_040.sh
+++ build_amiga_040.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-make USE_POLYMOST=0 USE_OPENGL=0 USE_ASM=0 CC="m68k-amigaos-gcc -noixemul -m68040 -m68881" CXX="m68k-amigaos-g++ -noixemul -m68040 -m68881" AS="vasm -Faout -quiet -phxass -m68040" AR=m68k-amigaos-ar RANLIB=m68k-amigaos-ranlib PLATFORM=AMIGA AMISUFFIX=".040" $*
+make USE_POLYMOST=0 USE_OPENGL=0 USE_ASM=0 CC="m68k-amigaos-gcc -noixemul -m68040 -m68881 -fbbb=- -fno-peephole2 -ffast-math" CXX="m68k-amigaos-g++ -noixemul -m68040 -m68881 -fbbb=- -fno-peephole2 -ffast-math" AS="vasm -Fhunk -quiet -phxass -m68040" AR=m68k-amigaos-ar RANLIB=m68k-amigaos-ranlib PLATFORM=AMIGA AMISUFFIX=".040" $*
--- build_amiga_060.sh
+++ build_amiga_060.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-make USE_POLYMOST=0 USE_OPENGL=0 USE_ASM=0 CC="m68k-amigaos-gcc -noixemul -m68060 -m68881" CXX="m68k-amigaos-g++ -noixemul -m68060 -m68881" AS="vasm -Faout -quiet -phxass -m68060 -DM68060" AR=m68k-amigaos-ar RANLIB=m68k-amigaos-ranlib PLATFORM=AMIGA AMISUFFIX=".060" $*
+make USE_POLYMOST=0 USE_OPENGL=0 USE_ASM=0 CC="m68k-amigaos-gcc -noixemul -m68060 -m68881 -fbbb=- -fno-peephole2 -ffast-math" CXX="m68k-amigaos-g++ -noixemul -m68060 -m68881 -fbbb=- -fno-peephole2 -ffast-math" AS="vasm -Fhunk -quiet -phxass -m68060 -DM68060" AR=m68k-amigaos-ar RANLIB=m68k-amigaos-ranlib PLATFORM=AMIGA AMISUFFIX=".060" $*
